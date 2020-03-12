7b0196373d6e2a0a85699373dd411a871f645f3c
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-12 01:57:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-   array.lenght >= 1
    line = "The line is currently: "
-  array.each.with do |i, person|
-    
-   end
+  array.each.with_index(1) do |person, i|
+    line << "#{i}. #{person}"
+    end
+    puts line
  end
  end

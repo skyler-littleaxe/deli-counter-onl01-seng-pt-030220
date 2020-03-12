c96bd189a5ab484e1a5bc9a37fa8ae5d849efe33
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-12 02:18:13 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+   line = "The line is currently:"
+  array.each.with_index(1) do |person, i|
+    line << " #{i}. #{person}"
+    end
+    puts line
+ end
+ end
+ 
+ def take_a_number(array, new_person)
+   array << new_person
+   puts "Welcome, #{new_person}. You are number #{array.length} in line."
+ end
+ 
+ def now_serving(array)
+ if array.empty?
+   puts "There is nobody waiting to be served!"
+ else
+   puts "Currently serving #{array.first}."
+   array.shift
+ end
+ end
+   
